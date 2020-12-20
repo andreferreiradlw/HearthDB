a9a36ff533a1c79628fa7a63640bf7d7a98eb505
Andre Ferreira <43468798+andreferreiradlw@users.noreply.github.com>
2020-12-20 10:43:37 +0000

    2020 updates (#1)
    * Deletes Firebase Config
    
    * Fixes Card Image 404 Issues
    
    * New Build
    
    * Travis CI Setup files
    
    * Deletes old build
    
    * Add docs folder to gitignore
    
    * Fixes Vulnerabilities
    
    * Fixes Build Errors

--- src/app/cards.model.ts
+++ src/app/cards.model.ts
@@ -41,60 +41,52 @@ export class Card {
   }
 
   getClassIcon() {
-    const path = '../../assets/class_icons/' + this.cardClass.toLowerCase() + '.png';
+    const path =
+      "../../assets/class_icons/" + this.cardClass.toLowerCase() + ".png";
     return path;
   }
 
   getFormattedSet() {
-    if (this.set === 'CORE') {
-      return 'Basic';
-    } else if (this.set === 'EXPERT1') {
-      return 'Classic';
-    } else if (this.set === 'HOF') {
-      return 'Hall of Fame';
-    } else if (this.set === 'NAXX') {
-      return 'Naxxramas';
-    } else if (this.set === 'GVG') {
-      return 'Goblins vs Gnomes';
-    } else if (this.set === 'BRM') {
-      return 'Blackrock Mountain';
-    } else if (this.set === 'TGT') {
-      return 'The Grand Tournament';
-    } else if (this.set === 'LOE') {
-      return 'The League of Explorers';
-    } else if (this.set === 'OG') {
-      return 'Whispers of the Old Gods';
-    } else if (this.set === 'KARA') {
-      return 'One Night in Karazhan';
-    } else if (this.set === 'GANGS') {
-      return 'Mean Streets of Gadgetzan';
-    } else if (this.set === 'UNGORO') {
-      return 'Journey to Un\'Goro';
-    } else if (this.set === 'ICECROWN') {
-      return 'Knights of the Frozen Throne';
-    } else if (this.set === 'LOOTAPALOOZA') {
-      return 'Kobolds & Catacombs';
-    } else if (this.set === 'GILNEAS') {
-      return 'The Witchwood';
-    } else if (this.set === 'BOOMSDAY') {
-      return 'The Boomsday Project';
-    } else if (this.set === 'TROLL') {
-      return 'Rastakhan\'s Rumble';
-    } else if (this.set === 'HERO_SKINS') {
-      return 'Hero Skins';
+    if (this.set === "CORE") {
+      return "Basic";
+    } else if (this.set === "EXPERT1") {
+      return "Classic";
+    } else if (this.set === "HOF") {
+      return "Hall of Fame";
+    } else if (this.set === "NAXX") {
+      return "Naxxramas";
+    } else if (this.set === "GVG") {
+      return "Goblins vs Gnomes";
+    } else if (this.set === "BRM") {
+      return "Blackrock Mountain";
+    } else if (this.set === "TGT") {
+      return "The Grand Tournament";
+    } else if (this.set === "LOE") {
+      return "The League of Explorers";
+    } else if (this.set === "OG") {
+      return "Whispers of the Old Gods";
+    } else if (this.set === "KARA") {
+      return "One Night in Karazhan";
+    } else if (this.set === "GANGS") {
+      return "Mean Streets of Gadgetzan";
+    } else if (this.set === "UNGORO") {
+      return "Journey to Un'Goro";
+    } else if (this.set === "ICECROWN") {
+      return "Knights of the Frozen Throne";
+    } else if (this.set === "LOOTAPALOOZA") {
+      return "Kobolds & Catacombs";
+    } else if (this.set === "GILNEAS") {
+      return "The Witchwood";
+    } else if (this.set === "BOOMSDAY") {
+      return "The Boomsday Project";
+    } else if (this.set === "TROLL") {
+      return "Rastakhan's Rumble";
+    } else if (this.set === "HERO_SKINS") {
+      return "Hero Skins";
     }
   }
 
   image() {
-    return 'https://raw.githubusercontent.com/schmich/hearthstone-card-images/4.12.2/rel/' + this.dbfId + '.png';
-  }
-
-  goldImage() {
-    return 'http://media.services.zam.com/v1/media/byName/hs/cards/enus/animated/' + this.id + '_premium.gif';
-  }
-
-  cardArt() {
-    // return 'https://art.hearthstonejson.com/v1/512x/' + this.id + '.jpg';
-    return 'https://art.hearthstonejson.com/v1/render/latest/enUS/512x/' + this.id + '.png';
+    return `https://art.hearthstonejson.com/v1/render/latest/enUS/512x/${this.id}.png`;
   }
 }
